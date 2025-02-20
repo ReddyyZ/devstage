3c4dbc7bd54efbb0c324fbb35c6c9f1b8e0abddf
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2025-02-20 16:25:02 -0300

    feat: create Invite page

---
+++ src/app/invite/invite-input.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { IconButton } from "@/components/icon-button";
+import { InputField, InputIcon, InputRoot } from "@/components/input";
+import { Copy, Link } from "lucide-react";
+
+export default function InviteLinkInput({
+  inviteLink,
+}: {
+  inviteLink: string;
+}) {
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(inviteLink);
+
+    alert("Copied to clipboard");
+  };
+
+  return (
+    <InputRoot>
+      <InputIcon>
+        <Link />
+      </InputIcon>
+      <InputField readOnly defaultValue={inviteLink} />
+      <IconButton className="-mr-2" onClick={copyToClipboard}>
+        <Copy />
+      </IconButton>
+    </InputRoot>
+  );
+}
